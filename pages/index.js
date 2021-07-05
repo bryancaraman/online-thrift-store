839707eefde2c3e80773f5c17ef9d65bcc99b258
Jacob Brooks <jbrooks2@godaddy.com>
2021-07-05 11:02:57 -0700

    Remove shop page/component

---
+++ pages/index.js
@@ -0,0 +1,12 @@
+/* eslint-disable no-unused-vars */
+import React from 'react';
+import Head from '../components/head';
+
+export const IndexPage = () => (
+  <div>
+    <Head title='Home'/>
+    <h1>Welcome!</h1>
+  </div>
+);
+
+export default IndexPage;
