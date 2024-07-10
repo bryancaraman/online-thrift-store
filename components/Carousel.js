2d028fdb86eff4cf38a99df7f957e8305b6ab0c8
YuneydyP <yparedes@godaddy.com>
2024-07-10 18:58:07 -0400

    Added homepage and externa css file

---
+++ components/Carousel.js
@@ -0,0 +1,40 @@
+import React, { useState } from 'react';
+import { motion, AnimatePresence } from 'framer-motion';
+import './main.css';
+
+const Carousel = ({ images }) => {
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  const handleNext = () => {
+    setCurrentIndex((prevIndex) =>
+      prevIndex + 1 === images.length ? 0 : prevIndex + 1
+    );
+  };
+
+  const handlePrevious = () => {
+    setCurrentIndex((prevIndex) =>
+      prevIndex - 1 < 0 ? images.length - 1 : prevIndex - 1
+    );
+  };
+
+  return (
+    <div className="carousel">
+      <div className="carousel-images">
+        <AnimatePresence>
+          <motion.img id='CarouselImages'
+            key={currentIndex}
+            src={images[currentIndex]}
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            exit={{ opacity: 0 }}
+            transition={{ duration: 0.5 }}
+          />
+        </AnimatePresence>
+      </div>
+      <button onClick={handlePrevious}>Previous</button>
+      <button onClick={handleNext}>Next</button>
+    </div>
+  );
+};
+
+export default Carousel;
